05c3512c2599c9732265fe99d246aa58278c25d4
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-12-29 02:33:55 -0800

    UPDATE: is_num prototype

--- monty.h
+++ monty.h
@@ -74,6 +74,7 @@ void exec_opcode(char *opcode);
 void push_op(stack_t **stack, unsigned int line_number);
 void pall_op(stack_t **stack, unsigned int line_number);
 size_t printStack(const stack_t *h);
+int is_num(char *s);
 /* Freeing */
 void freeStack(void);
 void free_dlistint(stack_t *head);
